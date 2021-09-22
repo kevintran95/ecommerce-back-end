af32be32b9763eb06773806d7c18ec1976bd4c32
Kevin Tran <kevin_tran95@yahoo.com>
2021-09-21 20:59:34 -0700

    added GET route to tag-routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,14 +3,35 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// GET route to find all tags
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Tag.findAll({
+      include: [{ model: Product }, { model: ProductTag }]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+// GET route to find a single tag
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }, { model: ProductTag }],
+      
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
@@ -26,3 +47,4 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
+
