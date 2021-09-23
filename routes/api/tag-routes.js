7e9812153d5a319fa28f88ab0bcee8be23a2ac4c
Kevin Tran <kevin_tran95@yahoo.com>
2021-09-22 20:44:31 -0700

    completed products-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,11 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }, { model: ProductTag }]
+      include: [{
+        model: Product,
+        through: ProductTag,
+        as: 'Product' 
+      }]
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -19,7 +23,11 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }, { model: ProductTag }],
+      include: [{ 
+          model: Product,
+         through: ProductTag, 
+         as: 'Product'
+        }]
       
     });
 
